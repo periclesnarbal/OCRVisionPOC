55404756c3bf14ed74ebd97785a333eb4d81f3fe
periclesnarbal <periclesnarbal@gmail.com>
2024-04-06 14:58:52 -0300

    Initial Commit

---
+++ OCRVisionPOC/OCRVisionPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRVisionPOCApp.swift
+//  OCRVisionPOC
+//
+//  Created by Péricles Narbal on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OCRVisionPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
